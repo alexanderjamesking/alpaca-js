69d2235787564129a819ff321b2cdc0a0b0163d3
Alex King <alex@alexanderjamesking.com>
2013-11-24 12:44:57 +0000

    removed obsolete files

--- test/pipeline.js
+++ test/pipeline.js
@@ -24,27 +24,18 @@ describe('Pipeline', function() {
       pipeline.processors.length.should.equal(1);
       pipeline.processors[0].should.be.a.Function;
     });
-
   });
 
   describe('process', function() {
     it('should call process on the processor', function(done) {
+      var processor = new TextAppender('one ');
+      var processSpy = sinon.spy(processor, "process");
 
-      var process = function(exchange, callback) {
-        callback(null, exchange);
-      };
-
-      var spy = sinon.spy(process);
-
-      var stubProcessor = {
-        process : spy
-      };
-
-      pipeline.addProcessor(stubProcessor);
+      pipeline.addProcessor(processor);
 
       pipeline.process(new Exchange(), function(err, data) {
-          spy.calledOnce.should.be.true;
-          spy.calledOn(stubProcessor).should.be.true;
+          processSpy.calledOnce.should.be.true;
+          processSpy.calledOn(processor).should.be.true;
           done();
       });
     });
@@ -53,11 +44,39 @@ describe('Pipeline', function() {
       pipeline.addProcessor(new TextAppender('one '));
       pipeline.addProcessor(new TextAppender('two '));
       pipeline.addProcessor(new TextAppender('three'));
-      pipeline.process(new Exchange(), function(err, data) {
-        data.message.body.should.equal('one two three');
+      pipeline.process(new Exchange(), function(exchange) {
+        exchange.message.body.should.equal('one two three');
         done();
       });
-    })
+    });
+
+    it('should halt on error and set the error on the exchange', function (done) {
+      var process = function(exchange, callback) {
+        callback(new Error('failed to process'), exchange);
+      };
+
+      var processorThatErrors = {
+        process : process
+      };
+
+      var appender1 = new TextAppender('one ');
+      var appender2 = new TextAppender('two');
+
+      var spy1 = sinon.spy(appender1, "process");
+      var spy2 = sinon.spy(appender2, "process");
+
+      pipeline.addProcessor(appender1);
+      pipeline.addProcessor(processorThatErrors);
+      pipeline.addProcessor(appender2);
+
+      pipeline.process(new Exchange(), function(exchange) {
+        spy1.calledOnce.should.be.true;
+        spy2.calledOnce.should.be.false;
+        exchange.error.should.be.an.Error;
+        exchange.error.message.should.equal('failed to process');
+        done();
+      });
+    });
   });
 
 });
